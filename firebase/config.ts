aff9d8bbdc2947aa1d9297f27531d244bc853100
Yasein Hosaam <yaseinhosaam74@gmail.com>
2026-04-19 15:05:09 +0200

    Use environment variables

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQUl5LmLyLf07-lCjyzWeAjz1ityY7PUo",
-  authDomain: "my-store-9d669.firebaseapp.com",
-  projectId: "my-store-9d669",
-  storageBucket: "my-store-9d669.firebasestorage.app",
-  messagingSenderId: "655622141675",
-  appId: "1:655622141675:web:4a3e0fb6bf995bdf89b0ae",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAQUl5LmLyLf07-lCjyzWeAjz1ityY7PUo",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "my-store-9d669.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "my-store-9d669",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "my-store-9d669.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "655622141675",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:655622141675:web:4a3e0fb6bf995bdf89b0ae",
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
